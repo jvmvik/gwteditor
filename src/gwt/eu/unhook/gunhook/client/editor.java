bdcc7d69b30e4158b86b08da9fdc30ad80dd0233
Victor Benarbia <jvmvik@gmail.com>
2010-08-22 19:54:39 +0200

    Add GWT support for CodeMirror

---
+++ src/gwt/eu/unhook/gunhook/client/editor.java
@@ -0,0 +1,14 @@
+package eu.unhook.gunhook.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class editor implements EntryPoint {
+    /**
+     * This is the entry point method.
+     */
+    public void onModuleLoad() {
+    }
+}
